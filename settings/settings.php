2b0f98c93fdf9ae7b3b954ae2b1d231b154654d0
TheBreasy <b.cautaerts@hotmail.be>
2021-03-14 16:25:52 +0100

    added db connection

---
+++ settings/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+const SETTINGS = [
+    "db" => [
+        "dbname" => "db_digital_dime",
+        "user" => "root",
+        "password" => "root",
+        "host" => "localhost",
+        "port" => "8889"
+    ]
+];
