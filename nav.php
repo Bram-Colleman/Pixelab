eeb4e6b4688061223c7c76dbae0a3f212b0fc34c
Bram Colleman <bram.colleman@telenet.be>
2021-04-29 21:03:40 +0200

    Added functionality for older php version + show avatars

--- nav.php
+++ nav.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    include_once(__DIR__."/classes/User.php");
+    try {
+        $user = User::fetchUser($_SESSION['email']);
+    } catch (Exception $e) {
+        $error = $e->getMessage();
+    }
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid" style="height: 5%">
         <a class="navbar-brand" href="./feed.php">Pixelab</a>
@@ -23,7 +32,13 @@
                 <li class="nav-item dropdown" style="height: 5%">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php if (!empty($user)) : ?>
+                            <?php if (!empty($user->getAvatar())) : ?>
+                                <img src="./uploads/avatars/<?php echo $user->getAvatar();?>" alt="" class="rounded-circle" style="max-width: 1.5rem">
+                            <?php else: ?>
                         <img src="./images/blank_avatar.png" alt="" class="rounded-circle" style="max-width: 1.5rem">
+                            <?php endif; ?>
+                        <?php endif; ?>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
                         <li><a class="dropdown-item" href="./profile.php">Profile</a></li>
