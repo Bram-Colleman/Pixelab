5eeb0e4db5b102256fb2a50d733a29747c6efd1f
Bram Colleman <bram.colleman@telenet.be>
2021-04-30 00:12:20 +0200

    Fixed settings issue

---
+++ settings/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+const SETTINGS = [
+    "db" => [
+        "dbname" => "db_project",
+        "user" => "root",
+        "password" => "root",
+        "host" => "localhost",
+        "port" => "3307"
+    ]
+];
