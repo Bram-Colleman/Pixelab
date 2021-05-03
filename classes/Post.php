771db2154f198f1616edaa7a5c390cbc8139f643
TheBreasy <b.cautaerts@hotmail.be>
2021-05-03 23:50:06 +0200

    added delete avatar functionality

--- classes/Post.php
+++ classes/Post.php
@@ -3,164 +3,181 @@
 include_once(__DIR__ . "/Db.php");
 include_once(__DIR__ . "/User.php");
 
- Class Post {
-     private $user;
-     private $image;
-     private $description;
-     private $timestamp;
-     private $likes = array();
-     private $comments = array();
+class Post
+{
+    private $user;
+    private $image;
+    private $description;
+    private $timestamp;
+    private $likes = array();
+    private $comments = array();
 
-     public function __construct($user = null, $image = null, $description = null, $timestamp = null, $likes = null, $comments = null)
-     {
-         $this->setUser($user);
-         $this->setImage($image);
-         $this->setDescription($description);
-         $this->setTimestamp($timestamp);
-         $this->setLikes($likes);
-         $this->setComments($comments);
-     }
+    public function __construct($user = null, $image = null, $description = null, $timestamp = null, $likes = null, $comments = null)
+    {
+        $this->setUser($user);
+        $this->setImage($image);
+        $this->setDescription($description);
+        $this->setTimestamp($timestamp);
+        $this->setLikes($likes);
+        $this->setComments($comments);
+    }
 
-     public static function fetchRecentPosts()
-     {
-         $conn = Db::getConnection();
-         $statement = $conn->prepare("SELECT p.id, username, image, description, timestamp FROM posts p JOIN users u ON u.id = p.user_id ORDER BY timestamp DESC  LIMIT 20");
-         $statement->execute();
+    private function setUser($user): void
+    {
+        $this->user = $user;
+    }
 
-         $posts = $statement->fetchAll();
-         if (!$posts) {
-             throw new Exception('There are no posts found');
-         }
-         $recentPosts = Array();
+    private function setImage($image): void
+    {
+        $this->image = $image;
+    }
 
-         foreach ($posts as $post) {
+    private function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    private function setTimestamp($timestamp): void
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    public static function fetchRecentPosts()
+    {
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("SELECT p.id, username, image, description, timestamp FROM posts p JOIN users u ON u.id = p.user_id ORDER BY timestamp DESC  LIMIT 20");
+        $statement->execute();
+
+        $posts = $statement->fetchAll();
+        if (!$posts) {
+            throw new Exception('There are no posts found');
+        }
+        $recentPosts = array();
+
+        foreach ($posts as $post) {
             array_push($recentPosts, new Post($post['username'], $post['image'], $post['description'], $post['timestamp'],
-                (empty(Post::fetchLikes($post['id'])))? array() : Post::fetchLikes($post['id']), (empty(Post::fetchComments($post['id']))) ? array() : Post::fetchComments($post['id'] )));
-         }
-         return $recentPosts;
+                (empty(Post::fetchLikes($post['id']))) ? array() : Post::fetchLikes($post['id']), (empty(Post::fetchComments($post['id']))) ? array() : Post::fetchComments($post['id'])));
+        }
+        return $recentPosts;
 
-     }
-     public static function fetchPostsByUserId($userId) {
-         $postUser = User::fetchUserByUserId($userId);
-         $conn = Db::getConnection();
-         $statement = $conn->prepare("SELECT * FROM posts WHERE user_id = :userId ORDER BY timestamp DESC");
-         $statement->bindValue(":userId", $userId);
-         $statement->execute();
+    }
 
-         $posts = $statement->fetchAll();
-         if (!posts) {
-             throw new Exception('Something went wrong!');
-         }
-         $fetchedPosts = array();
+    public static function fetchLikes($postId)
+    {
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("SELECT username FROM post_likes pl JOIN users u ON u.id = pl.user_id WHERE post_id = :postId");
+        $statement->bindValue(":postId", $postId);
+        $statement->execute();
+        $fetchedLikes = $statement->fetchAll();
+        $postLikes = array();
+        if (!empty($fetchedLikes)) {
+            foreach ($fetchedLikes as $fetchedLike) {
+                array_push($postLikes, $fetchedLike['username']);
+            }
+        }
+        return $postLikes;
+    }
 
-         foreach ($posts as $post) {
-             array_push($fetchedPosts, new Post($post['id'], $post['image'], $post['description'], $post['timestamp'],
-                 (empty(Post::fetchLikes($post['id'])))? array() : Post::fetchLikes($post['id']), (empty(Post::fetchComments($post['id']))) ? array() : Post::fetchComments($post['id'] )));
-         }
-         return $fetchedPosts;
-     }
-     public static function fetchLikes($postId){
-         $conn = Db::getConnection();
-         $statement = $conn->prepare("SELECT username FROM post_likes pl JOIN users u ON u.id = pl.user_id WHERE post_id = :postId");
-         $statement->bindValue(":postId", $postId);
-         $statement->execute();
-         $fetchedLikes = $statement->fetchAll();
-         $postLikes = array();
-         if (!empty($fetchedLikes)){
-             foreach ($fetchedLikes as $fetchedLike){
-                 array_push($postLikes, $fetchedLike['username']);
-             }
-         }
-         return $postLikes;
-     }
-     public static function fetchComments($postId){
-         $conn = Db::getConnection();
-         $statement = $conn->prepare("SELECT u.username, c.content FROM comments c JOIN users u ON u.id = c.user_id WHERE c.post_id = :postId");
-         $statement->bindValue(":postId", $postId);
-         $statement->execute();
-         $fetchedComments = $statement->fetchAll();
-         $postComments = array();
-         if (!empty($fetchedComments)){
-             foreach ($fetchedComments as $fetchedComment){
-                 array_push($postComments, array("username" => $fetchedComment['username'], "content" => $fetchedComment['content']));
-             }
-         }
-         return $postComments;
-     }
+    public static function fetchComments($postId)
+    {
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("SELECT u.username, c.content FROM comments c JOIN users u ON u.id = c.user_id WHERE c.post_id = :postId");
+        $statement->bindValue(":postId", $postId);
+        $statement->execute();
+        $fetchedComments = $statement->fetchAll();
+        $postComments = array();
+        if (!empty($fetchedComments)) {
+            foreach ($fetchedComments as $fetchedComment) {
+                array_push($postComments, array("username" => $fetchedComment['username'], "content" => $fetchedComment['content']));
+            }
+        }
+        return $postComments;
+    }
 
-     public function getUser()
-     {
-         return $this->user;
-     }
-     public function getImage()
-     {
-         return $this->image;
-     }
-     public function getDescription()
-     {
-         return $this->description;
-     }
-     public function getTimestamp()
-     {
-         return $this->timestamp;
-     }
-     public function getLikes(): array
-     {
-         return $this->likes;
-     }
-     public function getComments(): array
-     {
-         return $this->comments;
-     }
+    public static function fetchPostsByUserId($userId)
+    {
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("SELECT * FROM posts WHERE user_id = :userId ORDER BY timestamp DESC");
+        $statement->bindValue(":userId", $userId);
+        $statement->execute();
 
-     private function setUser($user): void
-     {
-         $this->user = $user;
-     }
-     private function setImage($image): void
-     {
-         $this->image = $image;
-     }
-     private function setDescription($description): void
-     {
-         $this->description = $description;
-     }
-     private function setTimestamp($timestamp): void
-     {
-         $this->timestamp = $timestamp;
-     }
-     public function setLikes(array $likes): void
-     {
-         $this->likes = $likes;
-     }
-     public function setComments(array $comments): void
-     {
-         $this->comments = $comments;
-     }
+        $posts = $statement->fetchAll();
+        if (!$posts) {
+            throw new Exception('Something went wrong!');
+        }
+        $fetchedPosts = array();
 
-     public static function uploadPost($userId, $description) {
-         try {
-             $fileName = User::fetchUserByUserId($userId)->getUsername() . "_" . date('YmdHis') . ".jpg";
-         } catch (Exception $e) {
-             echo "dhg";
-         }
-         $targetDir = "uploads/posts/";
-         $targetFile = $targetDir . basename($fileName);
+        foreach ($posts as $post) {
+            array_push($fetchedPosts, new Post($post['id'], $post['image'], $post['description'], $post['timestamp'],
+                (empty(Post::fetchLikes($post['id']))) ? array() : Post::fetchLikes($post['id']), (empty(Post::fetchComments($post['id']))) ? array() : Post::fetchComments($post['id'])));
+        }
+        return $fetchedPosts;
+    }
+
+    public static function uploadPost($userId, $description)
+    {
+        try {
+            $fileName = User::fetchUserByUserId($userId)->getUsername() . "_" . date('YmdHis') . ".jpg";
+        } catch (Exception $e) {
+            echo "dhg";
+        }
+        $targetDir = "uploads/posts/";
+        $targetFile = $targetDir . basename($fileName);
 //         $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-         if($_FILES["file"]["error"] == 4) {
+        if ($_FILES["file"]["error"] == 4) {
 //means there is no file uploaded
-             throw new Exception("This is not an image");
-         }
+            throw new Exception("This is not an image");
+        }
 
-         move_uploaded_file($_FILES["postImage"]["tmp_name"], $targetFile);
+        move_uploaded_file($_FILES["postImage"]["tmp_name"], $targetFile);
 
-         $conn = Db::getConnection();
-         $statement = $conn->prepare("INSERT INTO posts (user_id, image, description) VALUES (:userId, :image, :description)");
-         $statement->bindValue(":userId", $userId);
-         $statement->bindValue(":image", $fileName);
-         $statement->bindValue(":description", $description);
-         $statement->execute();
-     }
- }
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("INSERT INTO posts (user_id, image, description) VALUES (:userId, :image, :description)");
+        $statement->bindValue(":userId", $userId);
+        $statement->bindValue(":image", $fileName);
+        $statement->bindValue(":description", $description);
+        $statement->execute();
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    public function getLikes(): array
+    {
+        return $this->likes;
+    }
+
+    public function setLikes(array $likes): void
+    {
+        $this->likes = $likes;
+    }
+
+    public function getComments(): array
+    {
+        return $this->comments;
+    }
+
+    public function setComments(array $comments): void
+    {
+        $this->comments = $comments;
+    }
+}
