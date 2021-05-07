26759b2d44d2d1a5542a358c8f5711c1be9fdff2
TheBreasy <b.cautaerts@hotmail.be>
2021-05-07 13:46:52 +0200

    re-added fetchUserByEmail function

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    include_once(__DIR__."/classes/User.php");
-    include_once(__DIR__."/classes/Post.php");
+    include_once("./classes/User.php");
+    include_once("./classes/Post.php");
     try {
         $currentUser = User::fetchUserByEmail($_SESSION['email']);
     } catch (Exception $e) {
