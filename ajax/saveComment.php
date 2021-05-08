c7054a3199381244d180b3bb368d6cd03ba4775c
TheBreasy <b.cautaerts@hotmail.be>
2021-05-08 21:41:10 +0200

    added post comment functionality

---
+++ ajax/saveComment.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once(__DIR__ . "/../classes/Db.php");
+include_once(__DIR__ . "/../classes/User.php");
+include_once(__DIR__ . "/../classes/Comment.php");
+
+if (!empty($_POST)) {
+    session_start();
+
+    $user = new User();
+
+    $c = new Comment();
+    $c->setPostId($_POST['postId']);
+    $c->setContent($_POST['comment']);
+    $c->setUserId($_SESSION['userId']);
+
+    $c->save();
+
+    $response = [
+        'status' => 'success',
+        'body' => htmlspecialchars($c->getContent()),
+        'message' => 'Comment saved'
+    ];
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
