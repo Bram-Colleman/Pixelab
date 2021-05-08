feac38bd13e74e5b3958697b80d27fc3358a06f3
Bram Colleman <bram.colleman@telenet.be>
2021-05-08 21:36:31 +0200

    Added like functionality

---
+++ ajax/likepost.php
@@ -0,0 +1,34 @@
+<?php
+include_once ('../classes/Post.php');
+
+if (!empty($_POST)) {
+    session_start();
+
+    try {
+        $post = Post::fetchPostById($_POST['postId']);
+        $post->like();
+        $b = [  'amount' => sizeof(Post::fetchLikes($post->getId())),
+                'postid' => $post->getId()
+        ];
+
+        $response = [
+            'status' => 'success',
+            'body' => $b,
+            'message' => 'Post liked'
+        ];
+
+        header('Content-type: application/json');
+        echo json_encode($response);
+    } catch (Exception $e) {
+        $response = [
+            'status' => 'Failed',
+            'body' => 'Something went wrong.',
+            'message' => 'Something went wrong.'
+        ];
+        echo json_encode($response);
+    }
+
+
+}
+
+
