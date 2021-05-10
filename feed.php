20cb4041cf5b66e54db81696d44359f50a3561e2
JakeDarb <stijn.bouckaert2209@icloud.com>
2021-05-10 22:21:39 +0200

    setup[report]: get postId, userId

--- feed.php
+++ feed.php
@@ -111,7 +111,10 @@ if(empty($_POST)){
                                data-userid="<?php echo User::fetchUserByUsername($_SESSION['user'])->getId();?>">
                             <i class="fa <?php echo in_array($_SESSION['user'],$post->getLikes())? "fa-heart": "fa-heart-o";?> btn-icon" aria-hidden="true" ></i>
                         </a>
-                        <a href="#" class="border-0 outline-none bg-none text-blac btn-report">Report</a>
+                        <a href="#" class="border-0 outline-none bg-none text-blac btn-report" 
+                        data-postid="<?php echo $post->getId();?>" 
+                        data-userid="<?php echo User::fetchUserByUsername($_SESSION['user'])->getId();?>"
+                        >Report</a>
 <!--                    --><?php //var_dump(Post::fetchPostById(44)); ?>
                 </div>
                 <!--    likes:-->
--- js/liveReportPost.js
+++ js/liveReportPost.js
@@ -1,7 +1,11 @@
 let reportButtons = document.querySelectorAll(".btn-report");
 for (let i = 0; i < reportButtons.length; i++) {
     reportButtons[i].addEventListener('click', function(e) {
-        console.log("hey");
+        // Getting post id
+        let postId = this.dataset.postid;
+        let userId = this.dataset.userid;
+        console.log(postId+"   "+userId);
+
         e.preventDefault();
     });
 }
